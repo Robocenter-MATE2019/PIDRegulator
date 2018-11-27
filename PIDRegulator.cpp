0a43cadb18f5842fc7fb4fbadb3b7ce43c489415
Andrei-Mudrov-archmage <44801908+Andrei-Mudrov-archmage@users.noreply.github.com>
2018-11-27 18:39:11 +1000

    Add files via upload

---
+++ PIDRegulator.cpp
@@ -0,0 +1,66 @@
+#include "PIDRegulator.h"
+
+PIDRegulator::PIDRegulator(float pK, float iK, float dK)
+{
+	m_pK = pK;
+	m_dK = dK;
+	m_iK = iK;
+	m_timer.start();
+}
+
+int PIDRegulator::apply(float to_set, float current)
+{
+	if (is_first)
+	{
+		m_timer.start();
+		is_first = false;
+	}
+	float result;
+	float integral_part;
+	float proportional_part;
+	float differential_part;
+	float time_elaplsed = (m_timer.elapsed() / 1000.00) < 0.0000001 ? m_timer.elapsed() / 1000.00 + 0.000001 : m_timer.elapsed() / 1000.00;
+
+	m_timer.stop();
+
+	float error = to_set - current;
+
+	m_integral += error * time_elaplsed;
+
+	proportional_part = m_pK * error;
+
+	integral_part = m_iK * m_integral;
+
+	differential_part = m_dK * (error - m_error_old) / time_elaplsed;
+
+	result = proportional_part + integral_part + differential_part;
+
+	m_error_old = error;
+
+	m_timer.start();
+	result = constrain(result, -100, 100);
+	return result;
+}
+
+void PIDRegulator::set_k(float pK, float dK, float iK)
+{
+
+	m_pK = pK;
+	m_dK = dK;
+	m_iK = iK;
+}
+
+void PIDRegulator::set_pk(float pK)
+{
+	m_pK = pK;
+}
+
+void PIDRegulator::set_ik(float iK)
+{
+	m_iK = iK;
+}
+
+void PIDRegulator::set_dk(float dK)
+{
+	m_dK = dK;
+}
---
+++ PIDRegulator.h
@@ -0,0 +1,24 @@
+#include "Arduino.h"
+#include "Timer.h"
+
+using namespace std;
+
+class PIDRegulator
+{
+
+public:
+	PIDRegulator(float pK, float dK, float iK);
+	void set_k(float pK, float dK, float iK);
+	int apply(float to_set, float current);
+	void set_pk(float pk);
+	void set_ik(float ik);
+	void set_dk(float dk);
+private:
+	float m_error_old = 0;
+	float m_integral = 0;
+	float m_pK = 0;
+	float m_dK = 0;
+	float m_iK = 0;
+	bool is_first = true;
+	Timer m_timer;
+};
